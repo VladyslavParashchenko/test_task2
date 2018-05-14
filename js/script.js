47b4717fc768cb9992f2483b9dea24e570ef1861
vladyslavparashchenko <mrfenix2015@gmail.com>
2018-05-14 15:44:41 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -2,18 +2,29 @@ var header;
 var sticky;
 
 window.onload = function () {
-    window.onscroll = function () {
-    stickyFunction();
-};
+   window.onscroll = debounce(stickyFunction,100);
 }
 
 function stickyFunction() {
-    
-    header = document.querySelector(".top-menu");
+    header = document.querySelector(".menu-container");
     sticky = header.offsetTop;
     if (window.pageYOffset >= document.documentElement.clientHeight) {
         header.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
     }
+}
+
+function debounce(func, time) {
+    var timer = null;
+    return function (...args) {
+        var onTimerEnd = () => {
+            func.apply(this, args);
+            timer = null;
+        }
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(onTimerEnd, time);
+    };
 }
